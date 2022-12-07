e90aac64da7c01aa2dc454a2845230edb35df81e
Grucep23 <112145925+Grucep23@users.noreply.github.com>
2022-12-07 09:36:30 +0700

    static

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("haha")
+// const express = require('express')
+import express from 'express'
+import configViewEngine from './configs/viewEngine'
+
+const app = express();
+const port = 8080;
+
+configViewEngine(app);
+app.get('/haha', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
